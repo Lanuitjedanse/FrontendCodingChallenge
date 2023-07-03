1fc5ab7c5c72ade3437c3ac3fb4cd196888f134d
lucie bellenger <bellenger.lucie@gmail.com>
2023-07-03 15:56:48 +0200

    chore(BIKE24): configure storybook and add mui

--- bike24/src/app/page.tsx
+++ bike24/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>HELLO WORLD</div>
+      <div className="font-bold text-red-50">HELLO WORLD</div>
     </main>
   );
 }
